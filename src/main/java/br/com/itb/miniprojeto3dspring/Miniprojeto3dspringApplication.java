d10ccbbdfc9b7a7a8c2af210ac0ed80ba61a50b0
milyzinhagameplay <rm82986@estudante.fieb.edu.br>
2023-05-09 07:58:17 -0300

    pom alterado

---
+++ src/main/java/br/com/itb/miniprojeto3dspring/Miniprojeto3dspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3dspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojeto3dspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojeto3dspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itb/miniprojeto3dspring/Miniprojeto3dspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3dspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojeto3dspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
